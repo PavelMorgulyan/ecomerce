49254ac22e4a094e8bdf9782b86bbb492dc0c372
PavelMorgulyan <74529349+PavelMorgulyan@users.noreply.github.com>
2021-11-13 10:40:11 +0300

    Module 3 end

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,27 +1,27 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName('update-cart');
 
 for (i = 0; i < updateBtns.length; i++) {
 	updateBtns[i].addEventListener('click', function(){
-		var productId = this.dataset.product
-		var action = this.dataset.action
-		console.log('productId:', productId, 'Action:', action)
+		var productId = this.dataset.product;
+		var action = this.dataset.action;
+		console.log('productId:', productId, 'Action:', action);
 
-		console.log('USER:', user)
+		console.log('USER:', user);
 		if (user == 'AnonymousUser'){
-			console.log('User is not authenticated')
+			console.log('User is not authenticated');
 			
 		}else{
 			
-            updateUserOrder(productId, action)
+            updateUserOrder(productId, action);
 		}
 
 	})
 }
 
 function updateUserOrder(productId, action){
-	console.log('User is authenticated, sending data...')
+	console.log('User is authenticated, sending data...');
 
-		var url = '/update_item/'
+		var url = '/update_item/';
 
 		fetch(url, {
 			method:'POST',
@@ -30,10 +30,39 @@ function updateUserOrder(productId, action){
 			}, 
 			body:JSON.stringify({'productId':productId, 'action':action})
 		})
+
 		.then((response) => {
 		   return response.json();
 		})
+
 		.then((data) => {
-		    console.log('Data:', data)
+		    console.log('Data:', data);
 		});
+}
+
+function addCookieItem(productId, action){
+	console.log('User is not authenticated');
+
+	if(action == 'add'){
+		if(cart[productId] == undefined){
+			cart[productId]= {'quantity':1}
+		}
+		else{
+			cart[productId]['quantity'] += 1
+		}
+
+	}
+
+	if(action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if(cart[productId]['quantity'] <= 0 ){
+			console.log('Item should be deleted')
+			delete cart[productId]
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path/"
+	
+	location.reload()
 }
